f7892945cb330258879f0dc4233558fe7f8bbf0d
Sunil Rana <sunil.rana@mydala.com>
2017-10-31 22:19:58 +0530

    code clean

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # productlist_sunil
-Assignment
+
+This repository contains a assignment of good Android Architecture.
+
+Application Detail: 
+This application has 4 screens.
+Aim of app is to give a user feature to create a basic product list. User can update & delete product in product list.
+App follows MVVM project architecture.
+
+
+Libraries used in the project
+
+•	Design : AppCompat, CardView, RecyclerView ,DesignLibrary
+•	Reactive Programming : RxJava2 & RxAndroid
+•	Image Loading: Glide
+•	Json parsing: Gson
+•	Depending Injection: Dagger2
+•	View Injection: Butterknife
+•	Sqlite ORM: Room
+•	Android Architecture: MVVM
+•	Testing Tools: Robolectric, Junit, Mockito, Espresso
+
+
+
+The app has following packages:
+1.	dao: It contains a interface for database queries.
+2.	db: It contatins a abstract databse class
+3.	entity: Pojo class for product list.
+4.	repository: Interface & its implemenation for database async interaction  .
+5.	injection: Dependency providing classes using Dagger2.
+6.	ui.product: View classes along with their corresponding ViewModel.
+7.	utils: Utility classes.
+Classes have been designed in such a way that it could be inherited and maximize the code reuse.
--- app/src/main/java/com/sunil/demo/test/utils/Utils.java
+++ app/src/main/java/com/sunil/demo/test/utils/Utils.java
@@ -10,38 +10,6 @@ public class Utils {
 
 
     public static String loadJSONFromAsset(Context context) {
-        return  "[\n" +
-                "        {\n" +
-                "          \"id\": \"0\",\n" +
-                "          \"name\": \"Product 1\",\n" +
-                "          \"description\": \" 1 lorem ipsum detail\",\n" +
-                "          \"regular_price\": \"20\",\n" +
-                "          \"sale_price\": \"15\",\n" +
-                "          \"product_photo\": \"\",\n" +
-                "          \"color\": \"red\",\n" +
-                "          \"store\": \"A\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "          \"id\": \"1\",\n" +
-                "          \"name\": \"Product 2\",\n" +
-                "          \"description\": \" 2 lorem ipsum detail\",\n" +
-                "          \"regular_price\": \"50\",\n" +
-                "          \"sale_price\": \"40\",\n" +
-                "          \"product_photo\": \"\",\n" +
-                "          \"color\": \"green\",\n" +
-                "          \"store\": \"B\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "          \"id\": \"3\",\n" +
-                "          \"name\": \"Product 3\",\n" +
-                "          \"description\": \" 3 lorem ipsum detail\",\n" +
-                "          \"regular_price\": \"80\",\n" +
-                "          \"sale_price\": \"65\",\n" +
-                "          \"product_photo\": \"\",\n" +
-                "          \"color\": \"yellow\",\n" +
-                "          \"store\": \"C\"\n" +
-                "        }\n" +
-                "      ";
-
+        return "";
     }
 }
